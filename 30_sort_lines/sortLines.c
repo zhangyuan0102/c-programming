2edfb2849508a3319d3d775cecc9b1528dd769b7
Coursera Learner <nobody@nowhere.nul>
2024-02-25 01:49:37 +0000

    a

--- 30_sort_lines/sortLines.c
+++ 30_sort_lines/sortLines.c
@@ -15,10 +15,46 @@ int stringOrder(const void * vp1, const void * vp2) {
 void sortData(char ** data, size_t count) {
   qsort(data, count, sizeof(char *), stringOrder);
 }
+void printData(char ** data,size_t count){
+  for(int i=0;i<count;i++){
+    printf("%s",data[i]);
+    free(data[i]);
+  }
+}
+void getData(FILE *f){
+  char ** array =NULL;
+  char * line = NULL;
+  size_t sz =0;
+  size_t i=0;
+  while(getline(&line,&sz,f)>=0){
+    array = realloc(array,sz*sizeof(*array)+sizeof(*array));
+    array[i] = line;
+    line = NULL;
+    i++;
+  }
+  free(line);
+  sortData(array,i);
+  printData(array,i);
+  free(array);
+}
 
 int main(int argc, char ** argv) {
-  
-  //WRITE YOUR CODE HERE!
-  
-  return EXIT_SUCCESS;
+  if(argc ==1){
+    getData(stdin);
+  }
+  if(argc >1){
+    for(int i=1;i<argc;i++){
+            FILE * f = fopen(argv[i], "r");
+      if (f==NULL) {
+	fprintf(stderr, "File not found\n");
+	return EXIT_FAILURE;
+      }
+       getData(f);
+    if (fclose(f)!=0) {
+      fprintf(stderr, "File could not close properly\n");
+      return EXIT_FAILURE;
+    }
+  }
+  }
+    return EXIT_SUCCESS;
 }
