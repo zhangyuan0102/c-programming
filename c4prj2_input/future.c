68084f24a89ac8c955e1a0595505a552a408be01
Coursera Learner <nobody@nowhere.nul>
2024-03-11 02:13:30 +0000

    a

---
+++ c4prj2_input/future.c
@@ -0,0 +1,28 @@
+#include "future.h"
+#include "deck.h"
+void add_future_card(future_cards_t * fc, size_t index, card_t * ptr){
+        if (index >= fc->n_decks) {
+        fc->decks = realloc(fc->decks, (index + 1) * sizeof(deck_t));
+        for (size_t i = fc->n_decks; i <= index; i++) {
+            fc->decks[i] = (deck_t){ .cards = NULL, .n_cards = 0 };
+        }
+        fc->n_decks = index + 1;
+    }
+    add_card_to(fc->decks[index], *ptr);
+}
+void future_cards_from_deck(deck_t * deck, future_cards_t * fc){
+    if (deck->n_cards < fc->n_decks) {
+        fprintf(stderr, "Error: Not enough cards in the deck to fill future cards.\n");
+        return;
+    }
+    
+    for (size_t i = 0; i < fc->n_decks; i++) {
+        if (fc->decks[i].n_cards == 0) {
+            fprintf(stderr, "Error: Future card deck %zu is empty.\n", i);
+            continue;
+        }
+        for (size_t j = 0; j < fc->decks[i].n_cards; j++) {
+            *fc->decks[i].cards[j] = *deck->cards[j];
+        }
+    }
+}
---
+++ c4prj2_input/input.c
@@ -0,0 +1,47 @@
+#include "input.h"
+#include "future.h"
+#include <stdio.h>
+#include <string.h>
+deck_t * hand_from_string(const char * str, future_cards_t * fc){
+    deck_t *hand = malloc(sizeof(*hand));
+    hand->n_cards =0;
+    hand->cards = NULL;
+    while(str !=NULL && *str !='\0'){
+        char *end=strchr(str,' ');
+        char value = *str;
+        char suit = *(str+1);
+        if(value =='?'){
+         int index = atoi(str+1);
+         add_future_card(fc, index, add_empty_card(hand));   
+        }
+        else{
+           add_card_to(hand,card_from_letters(value,suit));
+        }
+        str = end +1;  
+    }
+    if(hand->n_cards<5){
+        fprintf(stderr, "Each hand should have at least 5 cards.\n");
+        exit(EXIT_FAILURE);
+    }
+  return hand;
+}
+
+deck_t ** read_input(FILE * f, size_t * n_hands, future_cards_t * fc){
+   deck_t ** ans =NULL;
+   *n_hands = 0;
+   ssize_t num=0;
+   size_t sz=0;
+  char *line = NULL;
+
+  while(num = getline(&line, &sz, f)!=-1){
+
+  line[num-1] = '\0';
+  
+  ans = realloc(ans,(*n_hands+1)*sizeof(*ans));
+  ans[*n_hands]=hand_from_string(line,fc);
+  (*n_hands)++;
+  }
+  free(line);
+  return ans;
+
+}
