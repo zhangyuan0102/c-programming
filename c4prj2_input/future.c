6b19dc94e14ec8ca52e5461eb40f8358398fb36a
Coursera Learner <nobody@nowhere.nul>
2024-03-11 03:01:28 +0000

    a

--- c4prj2_input/future.c
+++ c4prj2_input/future.c
@@ -1,11 +1,13 @@
 #include "future.h"
 #include "deck.h"
 #include <stdio.h>
+#include <assert.h>
 void add_future_card(future_cards_t * fc, size_t index, card_t * ptr){
-        if (index >= fc->n_decks) {
-        fc->decks = realloc(fc->decks, (index + 1) * sizeof(deck_t));
+        if (index+1 > fc->n_decks) {
+        fc->decks = realloc(fc->decks, (index + 1) * sizeof(*fc->decks));
         for (size_t i = fc->n_decks; i <= index; i++) {
-            fc->decks[i] = (deck_t){ .cards = NULL, .n_cards = 0 };
+	  fc->decks[i].cards = NULL;
+	  fc->deck[i].n_cards = 0 ;
         }
         fc->n_decks = index + 1;
     }
@@ -14,18 +16,16 @@ void add_future_card(future_cards_t * fc, size_t index, card_t * ptr){
     fc->decks[index].cards[fc->decks[index].n_cards - 1] = ptr;
 }
 void future_cards_from_deck(deck_t * deck, future_cards_t * fc){
-    if (deck->n_cards < fc->n_decks) {
-        fprintf(stderr, "Error: Not enough cards in the deck to fill future cards.\n");
-        return;
-    }
-    
-    for (size_t i = 0; i < fc->n_decks; i++) {
+        assert(deck->n_cards >= fc->n_decks);
+
+    int deck_index = 0;
+    for (int i = 0; i < fc->n_decks; i++) {
         if (fc->decks[i].n_cards == 0) {
-            fprintf(stderr, "Error: Future card deck %zu is empty.\n", i);
             continue;
         }
-        for (size_t j = 0; j < fc->decks[i].n_cards; j++) {
-            *fc->decks[i].cards[j] = *deck->cards[j];
+        for(int j = 0; j < fc->decks[i].n_cards; j++) {
+            *fc->decks[i].cards[j] = *deck->cards[deck_index]; 
         }
+        deck_index++;
     }
 }
--- c4prj2_input/input.c
+++ c4prj2_input/input.c
@@ -7,18 +7,22 @@ deck_t * hand_from_string(const char * str, future_cards_t * fc){
     hand->n_cards =0;
     hand->cards = NULL;
     while(str !=NULL && *str !='\0'){
-        char *end=strchr(str,' ');
-        char value = *str;
-        char suit = *(str+1);
-        if(value =='?'){
-         int index = atoi(str+1);
-         add_future_card(fc, index, add_empty_card(hand));   
+            if (*str == ' ') {
+            str++;
+            continue;
+        } else {
+            char value = *str;
+            str++;
+            if(value == '?') {
+                int index = atoi(str);
+                str = strchr(str, ' ');
+                add_future_card(fc, index, add_empty_card(hand));
+            } else {
+                char suit = *str;
+                str++;
+                add_card_to(hand, card_from_letters(value, suit));
+            }
         }
-        else{
-           add_card_to(hand,card_from_letters(value,suit));
-        }
-        str = end +1;  
-    }
     if(hand->n_cards<5){
         fprintf(stderr, "Each hand should have at least 5 cards.\n");
         exit(EXIT_FAILURE);
@@ -36,9 +40,9 @@ deck_t ** read_input(FILE * f, size_t * n_hands, future_cards_t * fc){
   while((num = getline(&line, &sz, f))!=-1){
 
   line[num-1] = '\0';
-  
-  ans = realloc(ans,(*n_hands+1)*sizeof(*ans));
-  ans[*n_hands]=hand_from_string(line,fc);
+  (*n_hands)++;
+  ans = realloc(ans,(*n_hands)*sizeof(*ans));
+  ans[*n_hands-1]=hand_from_string(line,fc);
   (*n_hands)++;
   }
   free(line);
