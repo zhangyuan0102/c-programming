65f7b8f0fb156131c12cf5a69ce5da8e093e18ee
Coursera Learner <nobody@nowhere.nul>
2024-03-11 03:37:04 +0000

    a

--- c4prj2_input/input.c
+++ c4prj2_input/input.c
@@ -1,52 +1,80 @@
-#include "input.h"
-#include "future.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-deck_t * hand_from_string(const char * str, future_cards_t * fc){
-    deck_t *hand = malloc(sizeof(*hand));
-    hand->n_cards =0;
-    hand->cards = NULL;
-    while(str !=NULL && *str !='\0'){
-            if (*str == ' ') {
-            str++;
-            continue;
-        } else {
-            char value = *str;
-            str++;
-            if(value == '?') {
-                int index = atoi(str);
-                str = strchr(str, ' ');
-                add_future_card(fc, index, add_empty_card(hand));
-            } else {
-                char suit = *str;
-                str++;
-                add_card_to(hand, card_from_letters(value, suit));
-            }
-        }
-    }
-    if(hand->n_cards<5){
-        fprintf(stderr, "Each hand should have at least 5 cards.\n");
-        exit(EXIT_FAILURE);
-    }
-  return hand;
-}
+#include <assert.h>
+#include "deck.h"
+#include "future.h"
+#include "input.h"
+#include "cards.h"
 
 deck_t ** read_input(FILE * f, size_t * n_hands, future_cards_t * fc){
-   deck_t ** ans =NULL;
-   *n_hands = 0;
-   ssize_t num=0;
-   size_t sz=0;
-  char *line = NULL;
+  assert(f!=NULL);
+  size_t num = 0;
+  size_t sz = 0;
+  char * line = NULL;
+  deck_t ** r_deck = NULL;
+  while(getline(&line, &sz, f)!=EOF){
 
-  while((num = getline(&line, &sz, f))!=-1){
-
-  line[num-1] = '\0';
-  (*n_hands)++;
-  ans = realloc(ans,(*n_hands)*sizeof(*ans));
-  ans[*n_hands-1]=hand_from_string(line,fc);
-  (*n_hands)++;
+    // create a hand from each valid line in file
+    r_deck=realloc(r_deck,sizeof(* r_deck)*(num+1));
+    deck_t * temp = hand_from_string(line,fc);
+    if (temp!=NULL){
+      // import "temp" to returned deck
+      r_deck[num]=temp;
+      num++;
+    }else{
+      // if line invalid go to next line or end if EOF
+      continue;
+    }
+    
+    
   }
+  * n_hands = num;
   free(line);
-  return ans;
+  return r_deck;
+}
+
+
+
+deck_t * hand_from_string(const char * str, future_cards_t * fc){
+  deck_t * deck=malloc(sizeof(*deck));
+  deck->cards=NULL;
+  deck->n_cards=0;
+
+  // analyse imported line
+  for (int i=0; i<strlen(str); i++){
+
+    // ignore characters space or newline
+    if(str[i] == ' ' || str[i] == '\n') continue;
+    
+    // if card is not unknown, add it
+    if(str[i] != '?') {
+      card_t card = card_from_letters(str[i], str[i+1]);
+      add_card_to(deck,card);
+      i++;
+    }else{
+      // if card is unknown add a future card
+      char str_n[strlen(str)];
+      int n=0;
+      i++;
+
+      // copy all card characters to the string up to next newline or space
+      while (!(str[i] == ' '|| str[i] == '\n' )){
+	str_n[n]=str[i];
+	i++;
+	n++;
+      }
+      // finish card string with terminator
+	str_n[n]= '\0';
+	add_future_card(fc, atoi(str_n), add_empty_card(deck));
+      }
+    }
+
+  // hands have to be minimum 5 cards
+  if (deck->n_cards < 5) {
+    fprintf(stderr, "Hands smaller than 5 cards");
+    return NULL;
+  }
+  return deck;
 
 }
