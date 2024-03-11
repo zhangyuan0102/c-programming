3ddfd5c0640a24495d71bb32e935de214aaff376
Coursera Learner <nobody@nowhere.nul>
2024-03-11 02:31:34 +0000

    a

--- c4prj2_input/future.c
+++ c4prj2_input/future.c
@@ -9,7 +9,9 @@ void add_future_card(future_cards_t * fc, size_t index, card_t * ptr){
         }
         fc->n_decks = index + 1;
     }
-	add_card_to(&(fc->decks[index)), *ptr);
+          fc->decks[index].n_cards++;
+    fc->decks[index].cards = realloc(fc->decks[index].cards, sizeof(*(fc->decks[index].cards)) * fc->decks[index].n_cards); 
+    fc->decks[index].cards[fc->decks[index].n_cards - 1] = ptr;
 }
 void future_cards_from_deck(deck_t * deck, future_cards_t * fc){
     if (deck->n_cards < fc->n_decks) {
